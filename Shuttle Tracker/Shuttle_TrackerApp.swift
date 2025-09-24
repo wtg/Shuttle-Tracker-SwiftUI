096e1eaa815f06910df34a26cb52efce2defc430
williamschen23 <chenwill2005@gmail.com>
2025-09-24 14:00:23 -0400

    Initial Commit

---
+++ Shuttle Tracker/Shuttle_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shuttle_TrackerApp.swift
+//  Shuttle Tracker
+//
+//  Created by Williams Chen on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Shuttle_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
