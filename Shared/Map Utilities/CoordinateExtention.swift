98392ea5d4f27ac7d3495dfb0f173975ca84a4ec
williamschen23 <chenwill2005@gmail.com>
2025-09-30 16:56:48 -0400

    initla map changes

---
+++ Shared/Map Utilities/CoordinateExtention.swift
@@ -0,0 +1,11 @@
+//
+//  CoordinateExtention.swift
+//  iOS
+//
+//  Created by Williams Chen on 9/27/25.
+//
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let RensselaerUnion = CLLocationCoordinate2DMake(42.73029109316892, -73.67655873298646)
+}
